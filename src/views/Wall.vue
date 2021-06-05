25ba02b61d08ab5ca0a4c09c2215ab6525fab216
Daltonic <darlingtongospel@gmail.com>
2021-06-05 12:18:52 +0100

    Sets up tailwind css and restuctures app

---
+++ src/views/Wall.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wall">
+    <h4>Welcome</h4>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Wall",
+  components: {
+  },
+};
+</script>
