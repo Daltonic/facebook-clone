05cb70ae986e495a3745ebf665b9fd3509c211f2
Daltonic <darlingtongospel@gmail.com>
2021-06-07 12:27:11 +0100

    adds sign in with google btn

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,46 @@
 <template>
   <div class="login grid place-items-center p-10">
-    <img src="../assets/logo.png" alt="Facebook Logo" />
-    <button
-      class="
-        my-5
-        p-3
-        rounded-full
-        text-white text-center
-        cursor-pointer
-        focus:outline-none
-      "
-    >
-      Login With Facebook
-    </button>
+    <img src="../assets/logo.png" alt="Facebook Logo" loading="lazy" />
+    <div class="flex items-center">
+      <button
+        class="
+          my-5
+          p-3
+          rounded-full
+          text-white text-center
+          cursor-pointer
+          focus:outline-none
+        "
+        @click="signInWithFacebook"
+      >
+        Sign In With Facebook
+      </button>
+
+      <button
+        class="
+          my-5
+          p-3
+          rounded-full
+          text-white text-center
+          cursor-pointer
+          focus:outline-none
+        "
+        @click="signInWithFacebook"
+      >
+        Sign In With Google
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "login",
+  methods: {
+    signInWithFacebook() {
+      console.log("Sign in with facebook");
+    },
+  },
 };
 </script>
 
@@ -29,14 +51,27 @@ export default {
   height: 200px;
 }
 
-.login > button {
+.login > div > button:first-child {
   border: 1px solid #45619d;
   background-color: #45619d;
   transition: all 0.3s ease-in-out;
+  margin: 20px 5px
 }
 
-.login > button:hover {
+.login > div > button:first-child:hover {
   background-color: transparent;
   color: #45619d;
+}
+
+.login > div > button:last-child {
+  border: 1px solid #0a8d48;
+  background-color: #0a8d48;
+  transition: all 0.3s ease-in-out;
+  margin: 20px 5px
+}
+
+.login > div > button:last-child:hover {
+  background-color: transparent;
+  color: #0a8d48;
 }
 </style>
