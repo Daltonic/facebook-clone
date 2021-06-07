35087e0fcbb4116686ec1456434908cdffd5434f
Daltonic <darlingtongospel@gmail.com>
2021-06-07 11:54:15 +0100

    add the login component

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    <img src="../asset/logo.png" alt="Facebook Logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+};
+</script>
+
+<style scoped>
+.login > img {
+  object-fit: contain;
+}
+</style>
