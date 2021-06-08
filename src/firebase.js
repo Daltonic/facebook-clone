4762d9739b5ed137f616ae5acbee5fb8e204e237
Daltonic <darlingtongospel@gmail.com>
2021-06-08 21:29:13 +0100

    adds up the firebase storage service

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/storage';
 import 'firebase/auth';
 import { firebaseConfig } from './app.config'
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore()
-const auth = firebaseApp.auth()
+const storage = firebaseApp.storage()  
+const auth = firebaseApp.auth()  
 const provider = new firebase.auth.GoogleAuthProvider()
 
-export { auth, provider } 
+export { auth, storage, provider } 
 export default db
