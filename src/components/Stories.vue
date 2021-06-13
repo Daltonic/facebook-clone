9bc2b581d62e3cded2982467e328a754531d515d
Daltonic <darlingtongospel@gmail.com>
2021-06-13 16:13:35 +0100

    removes store, changes the stories

--- src/components/Stories.vue
+++ src/components/Stories.vue
@@ -19,29 +19,30 @@ export default {
   setup() {
     const stories = ref([
       {
-        name: "Sonny Sangha",
-        src: "https://links.papareact.com/zof",
-        profile: "https://links.papareact.com/l4v",
+        name: "Darlington Gospel",
+        src: "https://firebasestorage.googleapis.com/v0/b/facebook-clone-dff86.appspot.com/o/stories%2FIMG-20191026-WA0000.jpg?alt=media&token=23ff54fb-ceb2-4476-a142-e886a32ebb60",
+        profile:
+          "https://firebasestorage.googleapis.com/v0/b/facebook-clone-dff86.appspot.com/o/stories%2FWhatsApp%20Image%202021-01-26%20at%2012.25.20%20PM.jpeg?alt=media&token=d3fe60ba-3e75-4748-9b4f-9b690c6f1426",
       },
       {
-        name: "Elon Musk",
-        src: "https://links.papareact.com/4zn",
-        profile: "https://links.papareact.com/kxk",
+        name: "Myles Munroe",
+        src: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2014/11/11/17/Myles_Munroe.jpg?width=1200",
+        profile: "https://1.bp.blogspot.com/-NVuQ02pDdMk/XcA7WZrOuGI/AAAAAAAAU1k/CJflEQ7wBiwOEfPsuUnYdMVH4ppCogXmQCLcBGAsYHQ/s1600/Myles%2B1.jpg",
       },
       {
-        name: "Jeff Bezoz",
-        src: "https://links.papareact.com/k2j",
-        profile: "https://links.papareact.com/f0p",
+        name: "TD Jakes",
+        src: "https://jakesdivinity.org/wp-content/uploads/2020/09/rsz_bishop_jakes_headshot-e1600264848251.jpg",
+        profile: "https://www1.cbn.com/sites/default/files/styles/original/public/media/standard/images/SUJA1_TD_Jakes_SI.jpg?itok=9Bg2WhZt",
+      },
+      {
+        name: "Kevin Zadia",
+        src: "https://kevinzadai.com/wp-content/uploads/2019/12/kevin-kathi-zadai-partnership.jpg",
+        profile: "https://yt3.ggpht.com/ytc/AAUvwngwW1v09FEB-mhdE2T6Jkqi0gdfbRpvEpBwCtce=s900-c-k-c0x00ffffff-no-rj",
       },
       {
         name: "Mark Zuckerberg",
         src: "https://links.papareact.com/xql",
         profile: "https://links.papareact.com/snf",
-      },
-      {
-        name: "Bill Gates",
-        src: "https://links.papareact.com/4u4",
-        profile: "https://links.papareact.com/zvy",
       },
     ]);
     return { stories };
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import './index.css'
 import { CometChat } from '@cometchat-pro/chat'
 import { cometChat } from './app.config'
@@ -29,7 +28,7 @@ CometChat.init(appID, appSetting)
   .then(() => {
     console.log('Initialization completed successfully')
     // You can now call login function.
-    createApp(App).use(store).use(router).mount('#app')
+    createApp(App).use(router).mount('#app')
   })
   .catch((error) => {
     console.log('Initialization failed with error:', error)
